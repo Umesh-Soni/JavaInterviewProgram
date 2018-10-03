71e79decfde4e6a144574b319f074de97e4cd6a1
Umesh Soni <112umesh.soni@gmail.com>
2018-10-03 18:56:07 +0530

    changes

--- src/test/JavaInterviewPrograms.java
+++ src/test/JavaInterviewPrograms.java
@@ -2,11 +2,55 @@ package test;
 
 public class JavaInterviewPrograms {
 
-	//Factorial program by recursion function
-	public int factorial(int number){
-		if(number == 0){
+	// Factorial :- Factorial program by recursion function
+	public int factorial(int number) {
+		if (number == 0) {
 			return 1;
 		}
-		return number*factorial(number-1);
+		return number * factorial(number - 1);
 	}
+
+	/*
+	 * Armstrong number :- A number is called an Armstrong number if it is equal
+	 * to the cube of its each digit
+	 */
+	public boolean armstrong(int num) {
+
+		int sum = 0, number = num;
+		while (number != 0) {
+			int quotient = 0;
+			quotient = number % 10;
+			number = number / 10;
+
+			sum = sum + (quotient * quotient * quotient);
+		}
+		if (sum == num)
+			return true;
+		else
+			return false;
+	}
+
+	/*
+	 * Integer Palindrome :- An integer is called palindrome if its equal to its
+	 * reverse e.g. 1001
+	 */
+	public boolean palindrome(int number) {
+		int value = 0, quotient = 0, num = number;
+
+		while (num != 0) {
+
+			quotient = num % 10;
+			num = num / 10;
+
+			value = value + quotient;
+			if (num != 0)
+				value = value * 10;
+		}
+		System.out.println(" num == number "+num+" == "+number);
+		if (value == number)
+			return true;
+		else
+			return false;
+	}
+
 }
--- src/test/JavaProgramTest.java
+++ src/test/JavaProgramTest.java
@@ -8,5 +8,23 @@ public class JavaProgramTest {
 		
 		//Calculate factorial with the help of recursion
 		System.out.println("Factorial of 6 is = "+obj.factorial(6));
+		
+		//Calculate armstrong number
+		System.out.println("153 is an armstrong number. "+obj.armstrong(153));
+		System.out.println("List of armstrong number in between 1 to 1000.");
+		for(int i=1;i <= 1000; i++){
+			if(obj.armstrong(i)){
+				System.out.print(i+", ");
+			}
+		}
+		
+		//Calculate palindrom number
+				System.out.println("\n1331 is an palindrom number. "+obj.palindrome(1331));
+				/*System.out.println("List of Palindrom number in between 1 to 1000.");
+				for(int i=1;i <= 1000; i++){
+					if(obj.palindrome(i)){
+						System.out.print(i+", ");
+					}
+				}*/
 	}
 }
